4075306c84a2ce83237e3c7c2c74980abfcb3eb4
Sakharin Buachan <sakharin@gmail.com>
2025-08-30 12:10:38 +0700

    test: create_user() should work

---
+++ tests/test_auth_models_user.py
@@ -0,0 +1,14 @@
+import pytest
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create_user(username="john", password="pass1234")
+
+    assert User.objects.count() == 1
+    user = User.objects.first()
+    assert user is not None
+    assert user.username == "john"
+    assert user.is_active is True
+    assert user.is_staff is False
