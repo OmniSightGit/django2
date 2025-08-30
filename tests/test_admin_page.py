3bdc199f4a6d160187868c1eeaecbd0108ad8292
Sakharin Buachan <sakharin@gmail.com>
2025-08-30 12:19:58 +0700

    test: can use the admin page as a regular user

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.models import User
 from django.test import Client
 from django.urls import reverse
 
@@ -10,3 +11,34 @@ def test_render_admin_login(client: Client):
     response = client.get(url)
 
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_admin_redirect_if_not_superuser(
+    client: Client, django_user_model: User
+):
+    user = django_user_model.objects.create_user(
+        username="john", password="pass1234"
+    )
+    client.force_login(user)
+    url = reverse("admin:index")
+
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.content == b""
+    assert response.headers["Location"] == "/admin/login/?next=/admin/"
+
+
+@pytest.mark.django_db
+def test_admin_success_for_superuser(client: Client, django_user_model: User):
+    user = django_user_model.objects.create_user(
+        username="john", password="pass1234", is_superuser=True, is_staff=True
+    )
+    client.force_login(user)
+    url = reverse("admin:index")
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert b"Django site admin" in response.content
