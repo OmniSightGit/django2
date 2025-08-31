da2ce8b8c18f2d31fc122410cda425fc5b961600
Sakharin Buachan <sakharin@gmail.com>
2025-08-31 11:27:28 +0700

    chore: configurate staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = Path(BASE_DIR).joinpath("staticfiles")
+STATICFILES_DIRS = (Path(BASE_DIR).joinpath("static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
